333f6be325b6f17c7e7604d719b5f6f49de4312b
tharun0678 <gtarun0678@gmail.com>
2023-08-18 16:18:15 +0000

    updated solution to return date

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const amountOfDays = require("date-fns/addDays");
 function numberOfDaysToAdd(days) {
   const result = amountOfDays(new Date(2020, 8, 22), days);
 
-  return result;
+  return `${result.getDate()}-${result.getMonth()}-${result.getFullYear()}`;
 }
-
-console.log(numberOfDaysToAdd(5));
 
 module.exports = numberOfDaysToAdd;
